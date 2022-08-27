baee011471ac1f2c7bab02731b763250431f2ad4
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-27 20:40:58 -0300

    variable reference

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 #![deny(clippy::all)]
 
-const NUMBER: i32 = 123; // constants declared upper case
-
 fn main() {
-    let name: &str = "leonardo";
-    let age: i32 = 20;
-    let age_complement: i32 = 4;
-    println!("some constant number: {}", NUMBER);
-    println!("age: {}", age + age_complement);
-    println!("Hello, world!  {}.", name);
+    let s1: String = String::from("john");
+    let s2: &String = &s1;
+    println!("hello, {}", s1);
+    println!("hello, {}", s2)
 }
