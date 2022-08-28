046b989f7c7293027d76c19ab3b46b73b2a2e49e
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-27 22:07:13 -0300

    return a string reference from a function

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@
 //     println!("hello, {}!", name);
 // }
 
-fn empty_string(value: &mut String) {
-    // clear the content of the input string
-    value.clear()
+// fn empty_string(value: &mut String) {
+//     // clear the content of the input string
+//     value.clear()
+// }
+
+fn get_name() -> &String {
+    &"John".to_string()
 }
 
 fn main() {
-    let mut name: String = String::from("john");
-    let name_ref_immutable: &String = &name;
-    let mut name_ref_muttable: &mut String = &mut name;
-    println!("{}", name);
-    println!("{}", name_ref_immutable);
-    println!("{}", name_ref_muttable);
-    // you cannot reference a variable as mutable while there are imotable references
+    let name = get_name();
 }
