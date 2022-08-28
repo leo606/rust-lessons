f07de4561341ed18b0792cec29623350192369c4
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-27 21:08:28 -0300

    mutable references

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ fn empty_string(value: &mut String) {
 
 fn main() {
     let mut name: String = String::from("john");
+    let name_ref_immutable: &String = &name;
+    let mut name_ref_muttable: &mut String = &mut name;
     println!("{}", name);
-    empty_string(&mut name);
-    println!("{}", name)
+    println!("{}", name_ref_immutable);
+    println!("{}", name_ref_muttable);
+    // you cannot reference a variable as mutable while there are imotable references
 }
