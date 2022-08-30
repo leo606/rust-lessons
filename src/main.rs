96e830f8fb1e627f3f3d9dd911da949b79ef6b2b
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-29 22:38:27 -0300

    inline function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let say_hello_to = |name: &str| format!("Hello, {} ", name);
-    println!("{}", say_hello_to("fulano"))
+    let full_name = |first_name: &str, last_name: &str| format!("{} {}", first_name, last_name);
+    println!("{}", full_name("john", "lennon"))
 }
