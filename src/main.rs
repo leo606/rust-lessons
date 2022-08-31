88916b721006ee573fe98e332a1b36c2faf46408
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-30 21:59:18 -0300

    mutable struct method

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,19 @@ impl Point {
     fn twice(&self) -> Point {
         Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
     }
+
+    fn twice_mut(&mut self) {
+        self.0 *= 2.0;
+        self.1 *= 2.0;
+        self.2 *= 2.0;
+    }
 }
 
 fn main() {
     let point: Point = Point(6.2, 3.2, 3.2);
     let point_two_times = point.twice();
-    println!("{}, {}, {}", point_two_times.0, point_two_times.1, point_two_times.2);
+    println!(
+        "{}, {}, {}",
+        point_two_times.0, point_two_times.1, point_two_times.2
+    );
 }
