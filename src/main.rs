1b96d65e777e285ec3af85663b33e9163ac3b7de
Leo Ferreira <leonardoferreira@protonmail.com>
2022-08-30 21:31:32 -0300

    structures

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![deny(clippy::all)]
 
-fn process_name(name: &str, callback: fn(&str) -> String) -> String {
-    callback(name)
+struct Person {
+    name: String,
+    age: u8,
 }
 
 fn main() {
-    let name_processed = process_name("john", |name: &str| name.to_uppercase());
-    println!("{}", name_processed)
+    let person: Person = Person {
+        name: "layne".to_string(),
+        age: 30,
+    };
+    println!("the person age {}, and name is {}", person.age, person.name)
 }
