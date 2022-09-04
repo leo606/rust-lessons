daef4b8de9eec32cb2ecff63fc80eb80de5eb38a
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-04 20:45:19 -0300

    enum instance

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
 #![deny(clippy::all)]
 
-#[derive(Debug)]
-
-struct Point(f64, f64, f64);
-
-impl Point {
-    fn describe(&self) {
-        println!("point is at ({}, {}, {})", self.0, self.1, self.2)
-    }
-
-    fn x_value(&self) -> f64 {
-        self.0
-    }
-
-    fn twice(&self) -> Point {
-        Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
-    }
-
-    fn twice_mut(&mut self) {
-        self.0 *= 2.0;
-        self.1 *= 2.0;
-        self.2 *= 2.0;
-    }
-
-    fn zero() -> Point {
-        Point(0.0, 0.0, 0.0)
-    }
+enum AnimalType { // enums always in pascal case
+    Dog, // and the properties too
+    Cat,
+    Rabbit
 }
 
 fn main() {
-    let point: Point = Point::zero();
-    println!("{:?}", point)
+    let my_dog: AnimalType = AnimalType::Dog;
 }
