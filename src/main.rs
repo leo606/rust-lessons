a4a1e3491cf59b4d1c22b902b1406c57d5586443
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-04 21:00:58 -0300

    animals match

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@
 
 #[derive(PartialEq)]
 
-enum AnimalType { // enums always in pascal case
+enum AnimalType {
+    // enums always in pascal case
     Dog, // and the properties too
     Cat,
-    Rabbit
+    Rabbit,
 }
 
 fn main() {
-    let my_dog: AnimalType = AnimalType::Dog;
-    if my_dog == AnimalType::Dog {
-        println!("is a dog")
+    let my_dog: AnimalType = AnimalType::Cat;
+    match my_dog {
+        AnimalType::Dog => println!("is a dog"),
+        AnimalType::Cat => println!("is a cat"),
+        AnimalType::Rabbit => println!("is a rabbit"),
     }
 }
