be44ae7fa2d4bb7e07a13c6b6b6008101bea91e3
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-05 21:23:49 -0300

    fixed length and type in array

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
 #![deny(clippy::all)]
 
-#[derive(PartialEq)]
-
-struct Point(f64, f64);
-
-enum Shapes {
-    Circle { radius: f64, center: Point },
-    Rectangle { width: f64, height: f64 },
-}
-
 fn main() {
-    let rectagle: Shapes = Shapes::Rectangle {
-        width: 3.0,
-        height: 4.0,
-    };
-
-    match rectagle {
-        Shapes::Rectangle {
-            width: 3.,
-            height: 4.,
-        } => println!("is equal"),
-        _ => println!("is not equal"),
-    }
+    let values_array: [&str; 2] = ["foo", "bar"];
+    println!("{}, {}", values_array[0], values_array[1])
 }
