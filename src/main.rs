4e818ad9d1819da2daaad2fee65c8290c4323cb2
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-05 21:57:24 -0300

    vector clear

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@
 fn main() {
     let mut values: Vec<i32> = vec![1, 2, 3];
     values.push(4);
-    let four = values.pop();
+    println!("values are {:?}", values);
+    values.clear();
+    println!("values are {:?}", values);
 }
