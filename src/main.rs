bd0f7d7d0de9a883fb489b6d7a99029ed5295edf
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-05 22:00:06 -0300

    extending a vector

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let mut values: Vec<i32> = vec![1, 2, 3];
     values.push(4);
     println!("values are {:?}", values);
-    values.clear();
+    values.extend_from_slice(&[5, 5, 5]);
     println!("values are {:?}", values);
 }
