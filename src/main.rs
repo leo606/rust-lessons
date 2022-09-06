978b12cf7cd66b7eeb1781c92cfed783f5362898
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-05 22:14:38 -0300

    test if vector contains a value

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut values1: Vec<i32> = vec![1, 2, 3];
-    let values2: Vec<i32> = vec![4, 5, 6];
-    println!("values1 {:?}", values1);
-    println!("values2 {:?}", values2);
-    values1.extend_from_slice(&values2);
-    println!("values1 {:?}", values1);
-    println!("values2 {:?}", values2);
+    let values1: Vec<i32> = vec![1, 2, 3];
+    if values1.contains(&9) {
+        println!("true")
+    } else {
+        println!("false")
+    }
 }
