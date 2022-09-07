adbc73ec927e0f0c20921999dbcb8d23a014118d
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-07 20:33:25 -0300

    insert value into hashmap if key is absent

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 #![deny(clippy::all)]
 
 use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 
 fn main() {
     let mut values: HashMap<&str, &str> = HashMap::new();
 
-    values.insert("foo", "bar");
-    values.insert("name", "this is a name");
+    values.insert("dog", "lyra");
+    
+    values.entry("cat").or_insert("noo");
 
-    let entry: Entry<&str, &str> = values.entry("foo");
-
-    match entry {
-        std::collections::hash_map::Entry::Occupied(value) => println!("{:?}", value.get()),
-        _ => println!("not found")
-    }
+    println!("{:?}", values)
 }
