6d616d1b0e0a3b2d71b9acf4b5a8bbef8a24dc11
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-07 21:31:01 -0300

    iteraion break - breaking out of iterations

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let values: Vec<&str> = vec!["john", "jane", "mary", "bob", "tom"];
 
     for name in values.iter() {
-        if name.len() != 3 {
-            continue;
+        if name.len() == 3 {
+            break;
         }
         println!("{}", name)
     }
