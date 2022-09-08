8e3753da42b80fd92363255a0f97b7713aa55cab
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-07 21:26:09 -0300

    vector iter filter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 #![deny(clippy::all)]
 
-use std::slice::Iter;
-
 fn main() {
-    let values: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let values: Vec<&str> = vec!["john", "jane", "mary", "bob", "tom"];
 
-    let values_iter: Iter<i32> = values.iter();
-
-    let values_double: Vec<i32> = values_iter.map(|value: &i32| value * 2).collect();
-
-    println!("{:?}", values_double);
+    for name in values.into_iter().filter(|n: &&str| n.len() == 3) {
+        println!("{}", name)
+    }
 }
