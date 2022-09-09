db3b2199b7e070eb649227e7dc0268145d6f5d7e
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-08 21:58:09 -0300

    unrap or else

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let age1: Option<i8> = Some(20);
-    let age2: Option<i8> = Some(30);
-    let age3: Option<i8> = Some(40);
+    let name: Option<&str> = None;
 
-    if let (Some(age_1), Some(age_2), Some(age_3)) = (age1, age2, age3) {
-        println!("{} {} {}", age_1, age_2, age_3)
-    }
+    let name_wrapped = name.unwrap_or_else(|| {
+        "default name"
+    });
+
+    println!("{}", name_wrapped)
 }
