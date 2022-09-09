f3b309f10943e4e541f9b6911a2eac62f291fe38
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-08 21:25:19 -0300

    unrapping multiple optionals with tuples

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let name: Option<&str> = Some("john");
+    let age1: Option<i8> = Some(20);
+    let age2: Option<i8> = Some(30);
+    let age3: Option<i8> = Some(40);
 
-    let name_unsafely_unrapped = name.expect("a horse with no name");
-
-    println!("{}", name_unsafely_unrapped);
+    if let (Some(age_1), Some(age_2), Some(age_3)) = (age1, age2, age3) {
+        println!("{} {} {}", age_1, age_2, age_3)
+    }
 }
