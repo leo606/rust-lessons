0c6d9aad585c216fd493f2e3d8c90bbb58f6232b
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-08 22:11:27 -0300

    option map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let name: Option<&str> = Some("sdf");
+    let age: Option<i32> = Some(2);
+    let age_multiplied_by_two = age.map(|age| age * 2);
+    println!("{}", age_multiplied_by_two.unwrap_or_default())
 
-    let b: bool = name.is_some();
-
-    println!("{}", b)
 }
