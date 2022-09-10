9d3ee36629d96ac579d3924746552052aeb34455
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-09 22:27:11 -0300

    mapping errors values

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,7 @@ fn get_full_name() -> Result<String, String> {
 
 fn main() {
     let full_name: Result<String, String> = get_full_name();
-    // let length = full_name.map(|name| name.len()).unwrap_or_default();
+    let length = full_name.map_err(|err_msg| err_msg.len());
 
-    match full_name {
-        Ok(name) => println!("{}", name),
-        Err(msg) => println!("Error: {}", msg)
-    }
+    println!("{:?}", length)
 }
