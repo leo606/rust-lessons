cb25e5ee3f35c0a572e468dae7cebefd50d3a930
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-09 21:58:42 -0300

    expecting an error from result

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![deny(clippy::all)]
 
 fn get_user_name() -> Result<String, ()> {
-    // Ok("some name".to_string())
-    Err(())
+    Ok("some name".to_string())
+    // Err(())
 }
 
 fn main() {
-    let user_name = get_user_name().expect("failed to get user name");
+    get_user_name().expect_err("not failed to get user name");
 
-    println!("hello, {}", user_name)
+    // println!("hello, {}", user_name)
 }
