f8e4b5c47eed508bee4b02d86f4807a3dd8cd055
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-09 22:23:40 -0300

    returning a error result as string

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 #![deny(clippy::all)]
 
-fn get_first_name() -> Result<String, ()> {
+fn get_first_name() -> Result<String, String> {
     // Ok("john".to_string())
-    Err(())
+    Err("there is no name".to_string())
 }
 
-fn get_last_name() -> Result<String, ()> {
+fn get_last_name() -> Result<String, String> {
     Ok("lennon".to_string())
     // Err(())
 }
 
-fn get_full_name() -> Result<String, ()> {
+fn get_full_name() -> Result<String, String> {
     let first_name: String = get_first_name()?;
     let last_name: String = get_last_name()?;
     Ok(format!("{} {}", first_name, last_name))
 }
 
 fn main() {
-    let full_name: Result<String, ()> = get_full_name();
-    let length = full_name.map(|name| name.len()).unwrap_or_default();
+    let full_name: Result<String, String> = get_full_name();
+    // let length = full_name.map(|name| name.len()).unwrap_or_default();
 
-    println!("{}", length)
+    match full_name {
+        Ok(name) => println!("{}", name),
+        Err(msg) => println!("Error: {}", msg)
+    }
 }
