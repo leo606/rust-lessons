5d167bf8b45d9a03ae266b0ca2f6faa0bed07c8c
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-10 22:22:26 -0300

    static value

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
 #![deny(clippy::all)]
 
-fn get_first_name() -> Result<String, String> {
-    // Ok("john".to_string())
-    Err("there is no name".to_string())
-}
-
-fn get_last_name() -> Result<String, String> {
-    Ok("lennon".to_string())
-    // Err(())
-}
-
-fn get_full_name() -> Result<String, String> {
-    let first_name: String = get_first_name()?;
-    let last_name: String = get_last_name()?;
-    Ok(format!("{} {}", first_name, last_name))
+fn get_full_name() -> &'static str { // adding "&'static" the string will live for the entirety of the application
+    "john lenno"
 }
 
 fn main() {
-    let full_name: Result<String, String> = get_full_name();
-    let length = full_name.map_err(|err_msg| err_msg.len());
-
-    println!("{:?}", length)
+    let full_name: &str = get_full_name();
+    println!("hello, {}", full_name)
 }
