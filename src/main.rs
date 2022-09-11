a1ff70f94f5981a6b8718eede1e646db78d2d9ee
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-10 22:31:21 -0300

    generic lifetime annotations

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![deny(clippy::all)]
 
-fn get_full_name() -> &'static str { // adding "&'static" the string will live for the entirety of the application
-    "john lenno"
+fn get_random_name<'lt1, 'lt2>(a: &'lt1 str, b: &'lt2 str) -> &'lt1 str { // adding "&'static" the string will live for the entirety of the application
+    a
 }
 
 fn main() {
-    let full_name: &str = get_full_name();
-    println!("hello, {}", full_name)
+    let random_name: &str = get_random_name("john", "doe");
+    println!("hello, {}", random_name)
 }
