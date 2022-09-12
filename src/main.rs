3f6c06b0756d21bac224210d7968e20ae6247500
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-12 20:54:24 -0300

    rust lifetime rules #2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 // lifetime rules
 // #1 - if you have parameters to your function that is references, the compiler automaticaly assings a lifetime operator to each one of them
+// #2 - single input lifetime is assigned to all outputs
 
 fn main() {
 
