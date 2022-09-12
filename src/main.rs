028fec6631bd85ca5b14646721e69598b79a4e57
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-12 20:56:05 -0300

    rust lifetime rules #3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // lifetime rules
 // #1 - if you have parameters to your function that is references, the compiler automaticaly assings a lifetime operator to each one of them
 // #2 - single input lifetime is assigned to all outputs
+// #3 - if &self or &mut self in parameters, lifetime of self is assigned to output
 
 fn main() {
 
