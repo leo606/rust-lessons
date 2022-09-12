a5d183de4e8966e28f5214634bad516296e9d1e6
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-12 20:40:21 -0300

    strucr params lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #![deny(clippy::all)]
 
-fn get_random_name<'lt1, 'lt2>(a: &'lt1 str, b: &'lt2 str) -> &'lt1 str { // adding "&'static" the string will live for the entirety of the application
-    a
+struct Person<'a> { // must define how long references will live for
+    name: &'a str,
 }
 
 fn main() {
-    let random_name: &str = get_random_name("john", "doe");
-    println!("hello, {}", random_name)
+
 }
