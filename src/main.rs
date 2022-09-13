feb3d39808ec1a3ab57b31239c9ba037df2393d2
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-12 21:09:26 -0300

    no referente return value in impl funcs

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,21 @@
 // #2 - single input lifetime is assigned to all outputs
 // #3 - if &self or &mut self in parameters, lifetime of self is assigned to output
 
-struct Person<'a>  {
-    fist_name: &'a str, // these two strings will live on for as long as that instance of person lives
+struct Person<'a> {
+    first_name: &'a str, // these two strings will live on for as long as that instance of person lives
     last_name: &'a str,
 }
 
-impl<'a> Person<'a> { // implementations of structs that have lifetime operators, also needs to have lifetimes operators
-    fn first_char_of_fist_name(&self) -> &str { // output will live as long self lives
-        &self.fist_name[0..1]
+impl<'a> Person<'a> {
+    // implementations of structs that have lifetime operators, also needs to have lifetimes operators
+    fn first_char_of_first_name(&self) -> &str {
+        // output will live as long self lives
+        &self.first_name[0..1]
+    }
+
+    fn get_full_name(&self) -> String { // return value is not a referense, so no need for lifetime operator, return value will live as long the func caller needs it to live
+        format!("{} {}", self.first_name, self.last_name)
     }
 }
 
-fn main() {
-
-}
+fn main() {}
