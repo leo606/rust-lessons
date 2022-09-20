74778a5bfe635f3caf4131ead9aa6cf4a4a804c8
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-20 08:27:15 -0300

    rc clone

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::rc::Rc;
 fn main() {
     let array = vec!["john".to_string(), "jane".to_string()];
     let rc = Rc::new(array);
+    let rc_clone = rc.clone();
     let weak = Rc::downgrade(&rc);
-    // println!("{:?} {:?}", rc, weak.upgrade().unwrap());
-    drop(rc);
     println!("{:?} {:?}", rc, weak.upgrade().unwrap());
+    drop(rc);
+    println!("{:?} {:?}", rc_clone, weak.upgrade().unwrap());
 }
