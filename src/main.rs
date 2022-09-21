0f190de8bc5b091fe02d357c407b1c232ebdd75d
Leo Ferreira <leonardoferreira@protonmail.com>
2022-09-20 21:52:15 -0300

    Cell value

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 #![deny(clippy::all)]
 
-use std::rc::Rc;
+use std::cell::Cell;
+
+struct Person {
+    name: String,
+    age: Cell<u8>,
+}
+
+impl Person {
+    fn increment_age(&self) {
+        self.age.set(self.age.get() + 1);
+    }
+}
 
 fn main() {
-    let array = vec!["john".to_string(), "jane".to_string()];
-    let rc = Rc::new(array);
-    let rc_clone = rc.clone();
-    let weak = Rc::downgrade(&rc);
-    println!("{:?} {:?}", rc, weak.upgrade().unwrap());
-    drop(rc);
-    println!("{:?} {:?}", rc_clone, weak.upgrade().unwrap());
+    let person = Person {
+        name: "john".to_string(),
+        age: Cell::new(20),
+    };
+
+    let age = person.age.get();
+
+    println!("{}", age);
+
+    person.increment_age();
+
+    let new_age = person.age.get();
+
+    println!("{}", new_age);
 }
