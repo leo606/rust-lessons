6b22a595f15f4fca6e23b8b40ba225207e389711
Leo Ferreira <leonardoferreira@protonmail.com>
2022-10-05 22:00:50 -0300

    rust async

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ use tokio::time::{sleep, Duration};
 
 async fn call_api_1() -> String {
     sleep(Duration::from_secs(3)).await;
-    "John".to_string()
+    "one".to_string()
+}
+
+async fn call_api_2() -> String {
+    sleep(Duration::from_secs(3)).await;
+    "two".to_string()
 }
 
 #[tokio::main]
 async fn main() {
     let name = call_api_1().await;
-    println!("{}", name)
+    println!("{}", name);
+
+    let name2 = call_api_2().await;
+    println!("{}", name2)
 }
